588f3d4d4ee484cd7bf48b94c1b965100865c020
Lorenzo Devichi <lorenzo.devichi@cbnpmp.fr>
2025-01-13 12:28:00 +0100

    second commit

---
+++ popup.js
@@ -0,0 +1,105 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const savedUrlsContainer = document.getElementById('savedUrlsContainer');
+
+    const displayUrls = (urls) => {
+        savedUrlsContainer.innerHTML = '';
+        if (urls && urls.length > 0) {
+            urls.forEach((entry) => {
+                const { id, url, description } = entry;
+
+                const urlLabel = document.createElement('div');
+                urlLabel.classList.add('url-item');
+
+                const urlDetails = document.createElement('div');
+                urlDetails.classList.add('url-details');
+                urlDetails.style.display = 'flex';
+                urlDetails.style.alignItems = 'center';
+
+                const urlInput = document.createElement('input');
+                urlInput.value = description || '';
+                urlInput.classList.add('url-input');
+                urlInput.style.display = 'none';
+
+                const urlDescription = document.createElement('p');
+                urlDescription.textContent = description || 'Ajouter une description';
+                urlDescription.classList.add('url-description');
+                urlDescription.style.marginRight = '10px';
+
+                const urlLink = document.createElement('a');
+                urlLink.href = url;
+                urlLink.target = '_blank';
+                urlLink.textContent = 'Link';
+
+                urlDetails.appendChild(urlDescription);
+                urlDetails.appendChild(urlInput);
+                urlDetails.appendChild(urlLink);
+
+                const urlButtons = document.createElement('div');
+                urlButtons.classList.add('url-buttons');
+
+                const editButton = document.createElement('button');
+                const editIcon = document.createElement('i');
+                editIcon.classList.add('fa-solid', 'fa-pen-to-square');
+                editButton.appendChild(editIcon);
+                editButton.classList.add('edit-button');
+
+                const deleteButton = document.createElement('button');
+                const deleteIcon = document.createElement('i');
+                deleteIcon.classList.add('fa-solid', 'fa-trash');
+                deleteButton.appendChild(deleteIcon);
+                deleteButton.classList.add('delete-button');
+
+                const saveButton = document.createElement('button');
+                const saveIcon = document.createElement('i');
+                saveIcon.classList.add('fa-solid', 'fa-floppy-disk');
+                saveButton.appendChild(saveIcon);
+                saveButton.classList.add('save-button');
+                saveButton.style.display = 'none';
+
+                urlButtons.appendChild(editButton);
+                urlButtons.appendChild(deleteButton);
+                urlButtons.appendChild(saveButton);
+
+                editButton.addEventListener('click', () => {
+                    urlDescription.style.display = 'none';
+                    urlInput.style.display = 'inline';
+                    saveButton.style.display = 'inline';
+                    editButton.style.display = 'none';
+                });
+                deleteButton.addEventListener('click', () => {
+                    const updatedUrls = urls.filter((entry) => entry.id !== id);
+                    chrome.storage.sync.set({ savedUrls: updatedUrls }, () => {
+                        displayUrls(updatedUrls);
+                    });
+                });
+
+                saveButton.addEventListener('click', () => {
+                    const newDescription = urlInput.value;
+                    if (newDescription !== description) {
+                        const updatedUrls = urls.map((entry) =>
+                            entry.id === id ? { ...entry, description: newDescription } : entry
+                        );
+                        chrome.storage.sync.set({ savedUrls: updatedUrls }, () => {
+                            displayUrls(updatedUrls); 
+                        });
+                    }
+                });
+
+                urlLabel.appendChild(urlDetails);
+                urlLabel.appendChild(urlButtons);
+
+                savedUrlsContainer.appendChild(urlLabel);
+            });
+        } else {
+            const noUrlsMessage = document.createElement('p');
+            noUrlsMessage.textContent = 'No saved URL.';
+            savedUrlsContainer.appendChild(noUrlsMessage);
+        }
+    };
+
+    chrome.storage.sync.get(['savedUrls'], (data) => {
+        const savedUrls = data.savedUrls || [];
+        displayUrls(savedUrls);
+    });
+});
---
+++ script.js
@@ -0,0 +1,157 @@
+
+function waitForElements(selector, callback, timeout = 10000) {
+  const startTime = Date.now();
+  const interval = setInterval(() => {
+    const elements = document.querySelectorAll(selector);
+    if (elements.length > 0) {
+      clearInterval(interval);
+      callback(elements);
+    }
+    if (Date.now() - startTime > timeout) {
+      clearInterval(interval);
+      console.error(`Timeout: Élément(s) "${selector}" introuvable(s).`);
+    }
+  }, 100);
+}
+
+waitForElements('.controls-center', (elements) => {
+
+  elements.forEach((targetDiv) => {
+    if (!targetDiv.querySelector('#save-filter-btn')) {
+      const btnSearch = document.querySelector('.search-btn');
+      const btnClear = document.querySelector('.clear-btn');
+      const saveButton = document.createElement('button');
+
+      btnSearch.addEventListener('click', function () {
+        saveButton.remove();
+        saveButton.textContent = 'Save Filter';
+        saveButton.id = 'save-filter-btn';
+        saveButton.style.backgroundColor = '#357938';
+        saveButton.style.color = 'white';
+        saveButton.style.border = 'none';
+        saveButton.style.cursor = 'pointer';
+        saveButton.style.marginLeft = '10px';
+
+        saveButton.addEventListener('mouseenter', function () {
+          saveButton.style.backgroundColor = '#329b37';
+        });
+
+        saveButton.addEventListener('mouseleave', function () {
+          saveButton.style.backgroundColor = '#357938';
+        });
+        if(!targetDiv.querySelector('#save-filter-btn')){
+          targetDiv.appendChild(saveButton);
+        }
+      });
+      saveButton.addEventListener('click', function () {
+        const urlPath = window.location.href;
+        console.log(urlPath);
+      });
+
+      btnClear.addEventListener('click', function () {
+        saveButton.remove();
+      });
+    }
+    const interval = setInterval(() => {
+      const urlPath = window.location.pathname;
+      const regex = /\/Standard\/(.+)/;
+
+      if (regex.test(urlPath)) {
+        if (!targetDiv.querySelector('#save-filter-btn')) {
+          const btnClear = document.querySelector('.clear-btn');
+          const saveButton = document.createElement('button');
+  
+          saveButton.textContent = 'Save Filter';
+          saveButton.id = 'save-filter-btn';
+          saveButton.style.backgroundColor = '#357938';
+          saveButton.style.color = 'white';
+          saveButton.style.border = 'none';
+          saveButton.style.cursor = 'pointer';
+          saveButton.style.marginLeft = '10px';
+
+          saveButton.addEventListener('click', function () {
+            const urlPathForSave = window.location.href;
+            const urlId = Date.now();
+        
+            chrome.storage.sync.get(['savedUrls'], (data) => {
+              const savedUrls = data.savedUrls || [];
+              savedUrls.push({ id: urlId, url: urlPathForSave });
+        
+              chrome.storage.sync.set({ savedUrls: savedUrls }, () => {
+                console.log('URL sauvegardée avec ID:', urlId);
+              });
+              alert("Filter saved. Click on the extension to view the filter.");
+            });
+          });
+        
+
+          saveButton.addEventListener('mouseenter', function () {
+            saveButton.style.backgroundColor = '#329b37';
+          });
+
+          saveButton.addEventListener('mouseleave', function () {
+            saveButton.style.backgroundColor = '#357938';
+          });
+
+          if(!targetDiv.querySelector('#save-filter-btn')){
+            targetDiv.appendChild(saveButton);
+          }
+         
+          btnClear.addEventListener('click', function () {
+            saveButton.remove();
+            console.log('Cleared');
+          });
+          
+        }
+        clearInterval(interval);
+      } else {
+        console.log("Rien après /Standard !");
+        clearInterval(interval);
+      }
+    }, 1000);
+  });
+
+  const targetNode = document.querySelector('.search-bar.search-advanced');
+  
+  const observer = new MutationObserver((mutationsList, observer) => {
+      for (let mutation of mutationsList) {
+        const saveButton = document.getElementById('save-filter-btn');
+        const btnSearch = document.querySelector('.search-btn');
+        saveButton.remove();
+        btnSearch.addEventListener('click', function () {
+
+          saveButton.textContent = 'Save Filter';
+          saveButton.id = 'save-filter-btn';
+          saveButton.style.backgroundColor = '#357938';
+          saveButton.style.color = 'white';
+          saveButton.style.border = 'none';
+          saveButton.style.cursor = 'pointer';
+          saveButton.style.marginLeft = '10px';
+
+          const divControlsCenter = document.querySelector('.controls-center');
+          
+          
+
+          setTimeout(() => {
+            if(!divControlsCenter.querySelector('#save-filter-btn')){
+              divControlsCenter.appendChild(saveButton);
+            }else{
+              console.log('Save button already exists');
+            }
+          }, 500);
+        });
+      }
+  });
+
+  
+  const config = { 
+      childList: true,
+      subtree: true,
+      attributes: true
+  };
+
+
+  observer.observe(targetNode, config);
+});
+
+
